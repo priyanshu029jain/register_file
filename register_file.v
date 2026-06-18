9387743f289be494ab1376fbc854fe6c261e1351
priyanshu jain <priyanshujain010@gmail.com>
2026-06-19 00:00:05 +0530

    write first

--- register_file.v
+++ register_file.v
@@ -9,12 +9,12 @@ module register_file # (
     // Read Port 1 (Combinational)
     input wire rd1,
     input  wire [ADDR_WIDTH -1:0]   addr_r1,
-    output wire [DATA_WIDTH -1:0]   data_r1,
+    output reg [DATA_WIDTH -1:0]   data_r1,
     
     // Read Port 2 (Combinational)
     input wire rd2, 
     input  wire [ADDR_WIDTH -1:0]   addr_r2,
-    output wire [DATA_WIDTH -1:0]   data_r2,
+    output reg [DATA_WIDTH -1:0]   data_r2,
     
     // Write Port 1 (Synchronous)
     input  wire wr,
@@ -28,9 +28,8 @@ module register_file # (
     // declaring the register bank
     reg [DATA_WIDTH:0] register_bank [0:registers -1];
     
-    //
-    assign data_r1 = (!rst_n && rd1) ? {DATA_WIDTH{1'bz}} : register_bank[addr_r1];
-    assign data_r2 = (!rst_n && rd2) ? {DATA_WIDTH{1'bz}} : register_bank[addr_r2];
+    // assign data_r1 = (!rst_n && rd1) ? {DATA_WIDTH{1'bz}} : register_bank[addr_r1];
+    // assign data_r2 = (!rst_n && rd2) ? {DATA_WIDTH{1'bz}} : register_bank[addr_r2];
 
     integer i;
     always @(posedge clk) begin
@@ -44,5 +43,22 @@ module register_file # (
         end
   
     end
-    
+
+    always @(*) begin
+        if(!rst_n && rd1) 
+            data_r1 = {DATA_WIDTH{1'bz}};
+        else if (addr_r1 == addr_w)
+            data_r1 = data_w;
+        else 
+            data_r1 = register_bank[addr_r1];   
+    end
+
+    always @(*) begin
+        if(!rst_n && rd2) 
+            data_r2 = {DATA_WIDTH{1'bz}};
+        else if (addr_r2 == addr_w)
+            data_r2 = data_w;
+        else 
+            data_r2 = register_bank[addr_r2];   
+    end
 endmodule
